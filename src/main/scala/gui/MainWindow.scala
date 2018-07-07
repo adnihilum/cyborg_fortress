8f1861efcbe2af1ceef0e18aaeb4871d23eb06ea
adnihilum <alex.adnihilum@gmail.com>
2018-07-07 16:22:11 +0500

    add test code for read keys as they are pressed

--- src/main/scala/gui/MainWindow.scala
+++ src/main/scala/gui/MainWindow.scala
@@ -1,11 +1,29 @@
 package gui
 import scala.swing._
+import scala.swing.event.{ButtonClicked, KeyPressed}
 
 trait MainWindow extends SimpleSwingApplication {
   def top : MainFrame = new MainFrame {
     title = "hello, world!"
-    contents = new Button {
-      text = "click me"
+
+    val label: Label = new Label {
+      text = "hello, world!"
     }
+
+    contents = new BoxPanel(Orientation.Horizontal) {
+      contents += label
+
+      listenTo(keys)
+      reactions += {
+        case key:KeyPressed =>
+          label.text = key.key.toString
+      }
+      focusable = true
+
+      requestFocus()
+    }
+
+
+
   }
 }
