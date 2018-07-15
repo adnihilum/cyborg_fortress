35ed2f80542bfe72ca0538c6a99a256c4164fd4c
adnihilum <alex.adnihilum@gmail.com>
2018-07-15 23:50:23 +0500

    create simple example with generating texture with pixel to pixel translation to screen

--- src/main/scala/gui/MainLwjgl.scala
+++ src/main/scala/gui/MainLwjgl.scala
@@ -11,8 +11,8 @@ import org.lwjgl.system.MemoryUtil._
 
 object MainLwjgl extends App {
 
-  private val WIDTH  = 800
-  private val HEIGHT = 600
+  private val width  = 800
+  private val height = 600
 
   def run() {
     try {
@@ -37,7 +37,7 @@ object MainLwjgl extends App {
     glfwWindowHint(GLFW_VISIBLE,   GLFW_FALSE) // hiding the window
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE) // window resizing not allowed
 
-    val window = glfwCreateWindow(WIDTH, HEIGHT, "LWJGL in Scala", NULL, NULL)
+    val window = glfwCreateWindow(width, height, "LWJGL in Scala", NULL, NULL)
     if (window == NULL)
       throw new RuntimeException("Failed to create the GLFW window")
 
@@ -47,8 +47,8 @@ object MainLwjgl extends App {
 
     glfwSetWindowPos (
       window,
-      (vidMode. width() -  WIDTH) / 2,
-      (vidMode.height() - HEIGHT) / 2
+      (vidMode. width() -  width) / 2,
+      (vidMode.height() - height) / 2
     )
 
     glfwMakeContextCurrent(window)
@@ -59,10 +59,7 @@ object MainLwjgl extends App {
   }
 
   private def loop(window: Long) {
-    GL.createCapabilities()
-
-    glClearColor(0f, 0f, 0f, 0f)
-
+    initRender()
     while (!glfwWindowShouldClose(window)) {
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
       render()
@@ -81,16 +78,73 @@ object MainLwjgl extends App {
       glfwSetWindowShouldClose(window, true)
   }
 
-  private var sp: Float = .5f
-  private def render(): Unit = {
-    glColor3f(0.0f, 1.0f, 0.0f)
-    glBegin(GL_QUADS)
-    glVertex2f(-sp, -sp)
-    glVertex2f(sp, -sp)
-    glVertex2f(sp, sp)
-    glVertex2f(-sp, sp)
-    glEnd()
+  val texId: Int = 0
+  private def next2powerValue(v: Int ): Int =
+    1 << (math.log(v.toDouble) / math.log(2.0D)).ceil.toInt
 
+  val widthTex: Int = next2powerValue(width)
+  val widthTexD: Double = width.toDouble / widthTex.toDouble
+  val heightTex: Int = next2powerValue(height)
+  val heightTexD: Double = height.toDouble / heightTex.toDouble
+
+  def initRender(): Unit = {
+    GL.createCapabilities()
+
+    glClearColor(0f, 0f, 0f, 0f)
+
+    // init projection
+    glMatrixMode(GL_PROJECTION)
+    glOrtho(0.0, width, 0.0, height, -width, height)
+    glMatrixMode(GL_MODELVIEW)
+
+    //init texture
+    val texId = glGenTextures()
+    glBindTexture(GL_TEXTURE_2D, texId)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    // Black/white checkerboard// Black/white checkerboard
+
+    val pixels = Array.ofDim[Float](3 * widthTex * heightTex)
+    for {
+      x <- 0 until widthTex
+      y <- 0 until heightTex
+      absIdx = x * 3 + y * 3 * widthTex
+      dIdx <- 0 until 3
+      idx = absIdx + dIdx
+    } {
+      pixels(idx) =
+        if((((x / 10).toInt % 2) + ((y / 10).toInt % 2)) % 2 == 0) 1.0f
+        else 0
+    }
+
+    glTexImage2D(
+      GL_TEXTURE_2D,
+      0,
+      GL_RGB,
+      widthTex,
+      heightTex,
+      0,
+      GL_RGB,
+      GL_FLOAT,
+      pixels)
+
+    glEnable(GL_TEXTURE_2D)
+  }
+
+  private def render(): Unit = {
+    glColor3f(1.0f, 1.0f, 1.0f)
+    glBegin(GL_QUADS)
+    glTexCoord2d(0, 0)
+    glVertex2d(0, 0)
+    glTexCoord2d(widthTexD, 0)
+    glVertex2f(width, 0)
+    glTexCoord2d(widthTexD, heightTexD)
+    glVertex2f(width, height)
+    glTexCoord2d(0, heightTexD)
+    glVertex2f(0, height)
+    glEnd()
   }
 
   run()
